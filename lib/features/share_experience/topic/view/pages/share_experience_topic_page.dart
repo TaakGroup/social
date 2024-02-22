b433c86f83c33057d4839d8d2e36bf49b8ae55df
imanmortezazadeh <imanmortezazadeh@ymail.com>
2024-02-22 12:24:19 +0330

    export app

--- lib/features/share_experience/topic/view/pages/share_experience_topic_page.dart
+++ lib/features/share_experience/topic/view/pages/share_experience_topic_page.dart
@@ -17,133 +17,138 @@ class ShareExperienceTopicPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      bottomNavigationBar: ShareExperienceInput(
-        onTap: TopicController.to.newExperience,
-        hintText: TopicController.to.topic.inputText,
-        avatar: TopicController.to.profile.avatarImage!,
-      ),
-      body: CustomScrollView(
-        controller: TopicController.to.scroll,
-        physics: const BouncingScrollPhysics(),
-        slivers: [
-          SliverAppBar(
-            floating: false,
-            snap: false,
-            pinned: true,
-            expandedHeight: 220,
-            leading: IconButton(
-              style: ElevatedButton.styleFrom(backgroundColor: context.colorScheme.background),
-              onPressed: Get.back,
-              icon: SvgPicture.asset(
-                AssetPaths.back,
-                color: context.colorScheme.onBackground,
-                height: 24,
-                width: 24,
+    return PopScope(
+      canPop: true,
+      onPopInvoked: (_) => Get.back(),
+      child: Scaffold(
+        bottomNavigationBar: ShareExperienceInput(
+          onTap: TopicController.to.newExperience,
+          hintText: TopicController.to.topic.inputText,
+          avatar: TopicController.to.profile.avatarImage!,
+        ),
+        body: CustomScrollView(
+          controller: TopicController.to.scroll,
+          physics: const BouncingScrollPhysics(),
+          slivers: [
+            SliverAppBar(
+              floating: false,
+              snap: false,
+              pinned: true,
+              expandedHeight: 220,
+              leading: IconButton(
+                style: ElevatedButton.styleFrom(backgroundColor: context.colorScheme.background),
+                onPressed: Get.back,
+                icon: SvgPicture.asset(
+                  AssetPaths.back,
+                  color: context.colorScheme.onBackground,
+                  height: 24,
+                  width: 24,
+                ),
               ),
-            ),
-            flexibleSpace: FlexibleSpaceBar(
-              title: const SizedBox(),
-              background: Stack(
-                children: [
-                  Positioned.fill(
-                    child: CachedNetworkImage(
-                      imageUrl: TopicController.to.topic.coverImage,
-                      fit: BoxFit.cover,
+              flexibleSpace: FlexibleSpaceBar(
+                title: const SizedBox(),
+                background: Stack(
+                  children: [
+                    Positioned.fill(
+                      child: CachedNetworkImage(
+                        imageUrl: TopicController.to.topic.coverImage,
+                        fit: BoxFit.cover,
+                      ),
                     ),
-                  ),
-                  Positioned(
-                    right: Decorations.paddingHorizontal,
-                    left: Decorations.paddingHorizontal,
-                    bottom: 12,
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          TopicController.to.topic.name,
-                          style: context.textTheme.displaySmall?.copyWith(color: context.colorScheme.background),
-                        ),
-                        const SizedBox(height: 4),
-                        if (TopicController.to.topic.bio.isNotEmpty)
+                    Positioned(
+                      right: Decorations.paddingHorizontal,
+                      left: Decorations.paddingHorizontal,
+                      bottom: 12,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
                           Text(
-                            TopicController.to.topic.bio,
-                            style: context.textTheme.labelLarge?.copyWith(
-                              color: context.colorScheme.background,
-                            ),
+                            TopicController.to.topic.name,
+                            style: context.textTheme.displaySmall?.copyWith(color: context.colorScheme.background),
                           ),
+                          const SizedBox(height: 4),
+                          if (TopicController.to.topic.bio.isNotEmpty)
+                            Text(
+                              TopicController.to.topic.bio,
+                              style: context.textTheme.labelLarge?.copyWith(
+                                color: context.colorScheme.background,
+                              ),
+                            ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              // Make the initial height of the SliverAppBar larger than normal.
+            ),
+            CupertinoSliverRefreshControl(
+              onRefresh: TopicController.to.onRefresh,
+            ),
+            TopicController.to.obx(
+              (model) => SliverPadding(
+                padding: const EdgeInsets.symmetric(vertical: 24, horizontal: Decorations.paddingHorizontal),
+                sliver: SliverList(
+                  delegate: SliverChildBuilderDelegate(
+                    (_, index) => Column(
+                      children: [
+                        ShareExperienceCommentWidget(
+                          comment: TopicController.to.items[index],
+                          onLike: TopicController.to.like,
+                          onDislike: TopicController.to.dislike,
+                          onComment: (comment) => TopicController.to.onComment(comment),
+                          onProfilePressed: (id) => Get.toNamed(AppRoutes.shareExperienceProfile,
+                              arguments: (id, TopicController.to.profile, TopicController.to.topics)),
+                        ),
+                        if (index + 1 == TopicController.to.items.length)
+                          Center(
+                            child: Obx(
+                              () => LoadingIndicatorWidget(
+                                isShow: TopicController.to.isPaginationLoading.value,
+                                color: context.colorScheme.primary,
+                                padding: const EdgeInsets.only(top: 16),
+                              ),
+                            ),
+                          )
+                        else
+                          const Divider(height: 32)
+                      ],
+                    ),
+                    childCount: TopicController.to.items.length,
+                  ),
+                ),
+              ),
+              onLoading: SliverList(
+                delegate: SliverChildBuilderDelegate(
+                  childCount: 1,
+                  (_, index) => Padding(
+                    padding: const EdgeInsets.all(32.0),
+                    child: Center(child: LoadingIndicatorWidget(color: context.colorScheme.primary)),
+                  ),
+                ),
+              ),
+              onEmpty: SliverList(
+                delegate: SliverChildBuilderDelegate(
+                  childCount: 1,
+                  (_, index) => SizedBox(
+                    height: context.height / 2,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Image.asset(AssetPaths.ticketsEmptyState, width: context.width * 0.3),
+                        const SizedBox(
+                          height: 16,
+                          width: double.infinity,
+                        ),
+                        Text('هنوز هیچ نظری برای این تالار ثبت نشده.', style: context.textTheme.bodyMedium),
                       ],
                     ),
                   ),
-                ],
-              ),
-            ),
-            // Make the initial height of the SliverAppBar larger than normal.
-          ),
-          CupertinoSliverRefreshControl(
-            onRefresh: TopicController.to.onRefresh,
-          ),
-          TopicController.to.obx(
-            (model) => SliverPadding(
-              padding: const EdgeInsets.symmetric(vertical: 24, horizontal: Decorations.paddingHorizontal),
-              sliver: SliverList(
-                delegate: SliverChildBuilderDelegate(
-                  (_, index) => Column(
-                    children: [
-                      ShareExperienceCommentWidget(
-                        comment: TopicController.to.items[index],
-                        onLike: TopicController.to.like,
-                        onDislike: TopicController.to.dislike,
-                        onComment: (comment) => TopicController.to.onComment(comment),
-                        onProfilePressed: (id) => Get.toNamed(AppRoutes.shareExperienceProfile, arguments: (id, TopicController.to.profile, TopicController.to.topics)),
-                      ),
-                      if (index + 1 == TopicController.to.items.length)
-                        Center(
-                          child: Obx(
-                            () => LoadingIndicatorWidget(
-                              isShow: TopicController.to.isPaginationLoading.value,
-                              color: context.colorScheme.primary,
-                              padding: const EdgeInsets.only(top: 16),
-                            ),
-                          ),
-                        )
-                      else
-                        const Divider(height: 32)
-                    ],
-                  ),
-                  childCount: TopicController.to.items.length,
                 ),
               ),
             ),
-            onLoading: SliverList(
-              delegate: SliverChildBuilderDelegate(
-                childCount: 1,
-                (_, index) => Padding(
-                  padding: const EdgeInsets.all(32.0),
-                  child: Center(child: LoadingIndicatorWidget(color: context.colorScheme.primary)),
-                ),
-              ),
-            ),
-            onEmpty: SliverList(
-              delegate: SliverChildBuilderDelegate(
-                childCount: 1,
-                (_, index) => SizedBox(
-                  height: context.height / 2,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Image.asset(AssetPaths.ticketsEmptyState, width: context.width * 0.3),
-                      const SizedBox(
-                        height: 16,
-                        width: double.infinity,
-                      ),
-                      Text('هنوز هیچ نظری برای این تالار ثبت نشده.', style: context.textTheme.bodyMedium),
-                    ],
-                  ),
-                ),
-              ),
-            ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
